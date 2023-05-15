76f56976a2862c0f9b77cbab351b87cc3ce5861f
oussama <oussama.ennadafy@gmail.com>
2023-05-15 15:19:15 +0100

    add compression package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const compression = require('compression')
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -14,11 +15,13 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(compression())
 app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
+app.use((req, res, next) =>
+{
   req.requestTime = new Date().getTime();
   next();
 });
@@ -27,7 +30,8 @@ app.use((req, res, next) => {
 app.use("/api/v1/meals", mealsRouter);
 app.use("/api/v1/orders", ordersRouter);
 
-app.all("*", (req, res, next) => {
+app.all("*", (req, res, next) =>
+{
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,26 +1,29 @@
 const AppError = require("./../utils/appError");
 
-const handleCastErrorDB = (err) => {
+const handleCastErrorDB = (err) =>
+{
   console.log(err);
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 
-const handleDuplicateFieldsDB = (err) => {
+const handleDuplicateFieldsDB = (err) =>
+{
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
-const handleValidationErrorDB = (err) => {
+const handleValidationErrorDB = (err) =>
+{
   const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
-const sendErrorDev = (err, res) => {
+const sendErrorDev = (err, res) =>
+{
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -29,7 +32,8 @@ const sendErrorDev = (err, res) => {
   });
 };
 
-const sendErrorProd = (err, res) => {
+const sendErrorProd = (err, res) =>
+{
   // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
@@ -50,7 +54,8 @@ const sendErrorProd = (err, res) => {
   }
 };
 
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res, next) =>
+{
   // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
